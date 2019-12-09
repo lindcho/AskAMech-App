f28fc5049e2e574e3d404823fd7d6020376aabe3
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2019-12-09 16:13:15 +0200

    refactor: remove unused namespaces

--- AskAMech.Infrastructure/Data/ApplicationDbContext.cs
+++ AskAMech.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using AskAMech.Domain.Models;
+﻿using AskAMech.Domain.Models;
+using AskAMech.Infrastructure.SeedData;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -18,8 +16,8 @@ namespace AskAMech.Infrastructure.Data
         {
             modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(iul => iul.UserId);
             modelBuilder.Entity<IdentityRole>().HasKey(r => r.Id);
-            modelBuilder.Entity<IdentityUserRole<string>>().HasKey(r => new {r.RoleId, r.UserId});
-            modelBuilder.Entity<IdentityUserToken<string>>().HasKey(r => new {r.UserId});
+            modelBuilder.Entity<IdentityUserRole<string>>().HasKey(r => new { r.RoleId, r.UserId });
+            modelBuilder.Entity<IdentityUserToken<string>>().HasKey(r => new { r.UserId });
             modelBuilder.Entity<Question>().HasData(QuestionsSeed.GenerateQuestions(15));
         }
         public DbSet<Question> Questions { get; set; }
--- AskAMech.Infrastructure/SeedData/QuestionsSeed.cs
+++ AskAMech.Infrastructure/SeedData/QuestionsSeed.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using AskAMech.Domain.Models;
 
-namespace AskAMech.Infrastructure
+namespace AskAMech.Infrastructure.SeedData
 {
     public class QuestionsSeed
     {
