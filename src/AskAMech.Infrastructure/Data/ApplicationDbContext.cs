d538c99d80c31ca9fe09ece6f7f1bfb0c9396137
samkelo <samkelo.zungu@chillisoft.co.za>
2020-02-06 10:40:51 +0200

    feat: seed Answers

--- src/AskAMech.Infrastructure/Data/ApplicationDbContext.cs
+++ src/AskAMech.Infrastructure/Data/ApplicationDbContext.cs
@@ -49,12 +49,14 @@ namespace AskAMech.Infrastructure.Data
                     {
                         new Question
                         {
+                            Id = 1,
                             Title = "where to buy a strong tire", Description = "I need to know where can i buy a strong tire.",
                             DateCreated = DateTime.Now.Subtract(TimeSpan.FromMinutes(60)),
                             LastModified = DateTime.Now.Subtract(TimeSpan.FromMinutes(10))
                         },
                         new Question
                         {
+                            Id = 2,
                             Title = "How to fit a tire",
                             Description = "I just bought a tire but don;t know how to fit it in my car.",
                             DateCreated = DateTime.Now.Subtract(TimeSpan.FromHours(3)),
@@ -62,27 +64,76 @@ namespace AskAMech.Infrastructure.Data
                         },
                         new Question
                         {
+                            Id = 3,
                             Title = "How to fit brackes", Description = "I bought brackpads but don't know how to fix it.",
                             DateCreated = DateTime.Now.Subtract(TimeSpan.FromDays(60)),
                             LastModified = DateTime.Now.Subtract(TimeSpan.FromDays(30))
                         },
                         new Question
                         {
+                            Id = 4,
                             Title = "CV problem", Description = "my cra is making a noise i think i need to replace cv's.",
                             DateCreated = DateTime.Now.Subtract(TimeSpan.FromDays(60)),
                             LastModified = DateTime.Now.Subtract(TimeSpan.FromMinutes(10))
                         },
                         new Question
                         {
+                            Id = 5,
                             Title = "My car brakes are not working",
                             Description = "i bought this brakes two months ago but it seams like they are not original.",
                             DateCreated = DateTime.Now.Subtract(TimeSpan.FromDays(60)),
                             LastModified = DateTime.Now.Subtract(TimeSpan.FromMinutes(16))
                         }
+                    },
+                    Answers = new[]
+                    {
+                        new Answer
+                        {
+                            AuthorId = "1f7999f-28be-4322-bc69-612ef8bbbb5c",
+                            QuestionId = 2,
+                            Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+                            Description = "fix it"
+                        },
+                        new Answer
+                        {
+                            AuthorId = "1f7999f-28be-4322-bc69-612ef8bbbb5c",
+                            QuestionId = 3,
+                            Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+                            Description = "fix it now"
+                        },
+                        new Answer
+                        {
+                            AuthorId = "1f7999f-28be-4322-bc69-612ef8bbbb5c",
+                            QuestionId = 4,
+                            Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+                            Description = "fix then test it"
+                        },
+                        new Answer
+                        {
+                            AuthorId = "36b2db51-238e-4224-ad1d-fe3077a5e4e4",
+                            QuestionId = 1,
+                            Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+                            Description = "You can buy from Continental! but the price is just off the roof.. Very expensive"
+                        },
+                        new Answer
+                        {
+                            AuthorId = "ffc183dc-78c2-413d-96f8-2091600c0b69",
+                            QuestionId = 1,
+                            Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+                            Description = "Super Tyres"
+                        },
+                        new Answer
+                        {
+                            AuthorId = "36b2db51-238e-4224-ad1d-fe3077a5e4e4",
+                            QuestionId = 1,
+                            Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(16)),
+                            Description = "Goodyear Tyres for new tires, but you can also try your new garage for second hand tyres"
+                        }
                     }
                 });
 
                 Set<ApplicationUser>().AddRange(defaultUser);
+                Set<Answer>().AddRange(Answers);
                 base.SaveChanges();
             }
         }
