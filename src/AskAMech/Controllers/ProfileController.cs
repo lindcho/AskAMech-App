b44b1825afa40e0903f14dd293531ba528fa1b72
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-02-24 14:56:24 +0200

    wip: add profile page to upload user image

---
+++ src/AskAMech/Controllers/ProfileController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+using AskAMech.Command.Services;
+using AskAMech.Domain.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace AskAMech.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IRequestUserProvider _requestUserProvider;
+        private IHostingEnvironment _environment;
+        private readonly IFileProvider fileProvider;
+
+        public ProfileController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IRequestUserProvider requestUserProvider, IHostingEnvironment environment, IFileProvider fileProvider)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _requestUserProvider = requestUserProvider;
+            _environment = environment;
+            this.fileProvider = fileProvider;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var userId = _requestUserProvider.GetUserId();
+            var user = await _userManager.FindByIdAsync(userId);
+            return View(user);
+        }
+
+        // GET: /Manage/ProfilePicture
+        public ActionResult ProfilePicture()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ProfilePicture(EmailForm model)
+        {
+            var userId = _requestUserProvider.GetUserId();
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (model == null ||
+                model.File == null || model.File.Length == 0)
+                return Content("file not selected");
+
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(), "wwwroot",
+                model.File.GetFilename());
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await model.File.CopyToAsync(stream);
+                user.PictureUrl = stream.ToString();
+            }
+
+
+            var updateProfileResult = await _userManager.UpdateAsync(user);
+            if (!updateProfileResult.Succeeded)
+            {
+                throw new InvalidOperationException($"Unexpected error ocurred updating the profile for user with ID '{user.Id}'");
+            }
+
+
+            return View();
+        }
+
+        public class EmailForm
+        {
+            /// <summary>
+            /// Gets or Sets the FileName
+            /// </summary>
+            [Required(ErrorMessage = "FileName is required")]
+            public string FileName { get; set; }
+
+            [Required(ErrorMessage = "File is required")]
+            [DataType(DataType.Upload)]
+            [FromForm(Name = "File")]
+            public IFormFile File { get; set; }
+        }
+    }
+}
---
+++ src/AskAMech/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using HeyRed.MarkdownSharp;
+
+namespace BlogPlayground.TagHelpers
+{
+    public class MarkdownTagHelper : TagHelper
+    {
+        public string Source { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //add wrapper span with well known class
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class", "markdown-contents");
+
+            //Add processed markup as the inner contents. Notice the usage of SetHtmlContent so the processed markup is not encoded
+            var markdown = new Markdown();           
+            output.Content.SetHtmlContent(markdown.Transform(this.Source));
+        }        
+    }
+}
