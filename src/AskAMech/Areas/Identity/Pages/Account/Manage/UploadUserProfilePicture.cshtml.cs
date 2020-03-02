d5791d43a8e60f941803272083fcd9e1c540b2d7
Khanyisile <khanyisile.mathe@chillisoft.co.za>
2020-03-02 11:27:54 +0200

    refactor: move profile picture validation from razor view to page view.

--- src/AskAMech/Areas/Identity/Pages/Account/Manage/UploadUserProfilePicture.cshtml.cs
+++ src/AskAMech/Areas/Identity/Pages/Account/Manage/UploadUserProfilePicture.cshtml.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
 using AskAMech.Command.Questions;
+using AskAMech.Controllers;
+using AskAMech.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,6 +16,8 @@ namespace AskAMech.Areas.Identity.Pages.Account.Manage
         {
             _questionCommands = questionCommands;
         }
+        [TempData]
+        public string StatusMessage { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -29,9 +33,27 @@ namespace AskAMech.Areas.Identity.Pages.Account.Manage
         
         public async Task<ActionResult> OnPost(IFormFile file)
         {
-            if (file != null && file.Length > 0)
+            if (file == null || file.Length == 0)
+            {
+                ModelState.AddModelError("", "Uploaded file is empty or null.");
+                return Page();
+            }
+
+            var isImage = file.IsImage();
+
+            if (!isImage)
+            {
+                ModelState.AddModelError("", "This photo extension is not allowed!");
+                return Page();
+            }
+
+            if (!ModelState.IsValid)
+                return Redirect("/Identity/Account/Manage");
+
+            if (file.Length > 0)
             {
                 await _questionCommands.UploadImage(file);
+                StatusMessage = "Your profile picture has been updated";
             }
             return Redirect("/Identity/Account/Manage");
         }
