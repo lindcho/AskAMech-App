14e67a84fc62307aacf8da14ceec57ef102dbceb
Khanyisile <khanyisile.mathe@chillisoft.co.za>
2020-03-03 14:47:41 +0200

    feat: add  Answers viewmodel

---
+++ src/AskAMech.Domain/AnswersListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AskAMech.Domain
+{
+   public class AnswersListViewModel
+    {
+        public string AnswerDescription { get; set; }
+        public DateTime DateAnswered { get; set; }
+    }
+}
