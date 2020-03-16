74b1647641addce030e4dc58fef61fa3b1b4bdae
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-03-16 13:39:12 +0200

    test: add tests for answer Question

--- src/AskAMech.Command.Test/Builders/AnswerQuestionBuilder.cs
+++ src/AskAMech.Command.Test/Builders/AnswerQuestionBuilder.cs
@@ -2,6 +2,7 @@
 using AskAMech.Command.Services;
 using AskAMech.Data.DbGateways.Answers;
 using AskAMech.Data.DbGateways.Questions;
+using AskAMech.Domain.Models;
 using NSubstitute;
 
 namespace AskAMech.Command.Test.Builders
@@ -16,7 +17,7 @@ namespace AskAMech.Command.Test.Builders
         {
             _answerGateway = Substitute.For<IAnswerGateway>();
             _questionGateway = Substitute.For<IQuestionGateway>();
-            _requestUserProvider = Substitute.For<RequestUserProvider>();
+            _requestUserProvider = Substitute.For<IRequestUserProvider>();
         }
 
         public AnswersCommand Build()
@@ -24,6 +25,21 @@ namespace AskAMech.Command.Test.Builders
             return new AnswersCommand(_answerGateway, _questionGateway, _requestUserProvider);
         }
 
+        public AnswerQuestionBuilder WithInvalidUserId(string userid)
+        {
+            _requestUserProvider.GetUserId().Returns(userid);
+            return this;
+        }
+        public AnswerQuestionBuilder WithQuestion(Question question)
+        {
+            _questionGateway.GetQuestion(question.Id).Returns(question);
+            return this;
+        }
 
+        public AnswerQuestionBuilder WithGeneratedUserId()
+        {
+             _requestUserProvider.GetUserId().Returns("1f7999f-28be-4322-bc69-612ef8cccc5c");
+             return this;
+        }
     }
 }
