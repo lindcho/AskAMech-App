ffd221f412a1d6650324a83bdd8069443e8abac8
Lindokuhle Ndaba <lindokuhle.ndaba@chillisoft.co.za>
2020-04-12 19:53:31 +0200

    chore: remove white spaces in TestUserProfile

--- src/AskAMech.Command.Test/Profile/TestUserProfile.cs
+++ src/AskAMech.Command.Test/Profile/TestUserProfile.cs
@@ -20,17 +20,11 @@ namespace AskAMech.Command.Test.Profile
             [Test]
             public async void GetUserByUserId_GivenUserId_ShouldReturnUserProfileDetailsAsync()
             {
-
                 // Arrange
-
                 var userId = "1f7999f-28be-4322-bc69-612ef8bbbb5c";
-
                 //Act
-
                 var user = await _requestUserProvider.GetUserByUserId(userId);
-
                 //Assert
-
                 Assert.That(user, Is.Not.Null);
                 Assert.That(user.FullName, Is.EqualTo("Khanyisile"));
                 Assert.That(user.Email, Is.EqualTo("askamech@gmail.com"));
@@ -41,17 +35,11 @@ namespace AskAMech.Command.Test.Profile
             [Test]
             public async void GetUserQuestions_GivenUserId_ShouldReturnUserQuestions()
             {
-
                 // Arrange
-
                 var userId = "1f7999f-28be-4322-bc69-612ef8bbbb5c";
-
                 //Act
-
                 var questions = await _questionCommands.GetUserQuestions(userId);
-
                 //Assert
-
                 Assert.That(questions, Is.Not.Null);
                 Assert.That(questions.Count, Is.EqualTo(1));
                 Assert.That(questions.GetType(), Is.EqualTo(typeof(List<Question>)));
@@ -61,20 +49,13 @@ namespace AskAMech.Command.Test.Profile
         [Test]
         public void GetQuestionsWithAnswers_GivenUserId_ShouldReturnQuestionsWithAnswers()
         {
-
             // Arrange
-
             var userId = "1f7999f-28be-4322-bc69-612ef8bbbb5c";
-
             //Act
-
             var questionAnswers = _answersCommand.GetQuestionsWithAnswers(userId);
-
             //Assert
-
             Assert.That(questionAnswers, Is.Not.Null);
             Assert.That(questionAnswers.GetType(), Is.EqualTo(typeof(System.Linq.IQueryable<AnswersListViewModel>)));
-
         }
 
     }
